4a03d1d1742296443d972dec65eb9dbfeacd410c
Moonshiness554 <sasha.jaffarove@gmail.com>
2013-08-10 16:48:53 -0700

    Update and rename README.md to Repository.ino

---
+++ Repository.ino
@@ -0,0 +1,18 @@
+How To Create A Repository On GitHub
+====================================
+
+1.Sign in to Github.
+
+2.Right next to your account name there should be a small book with a plus on it.
+
+3.Next you should see a box that says(Repository name).
+
+4.Fill that box in with a creative short name.
+              
+                
+                * DONT FORGET TO CHECK THE (Initialize this repository with a README)BOX
+                or you cant edit your repo later!
+
+5.Then click on the(Create Repository) button.
+
+6.Finally click the (README.md) and you click edit and start typing.
